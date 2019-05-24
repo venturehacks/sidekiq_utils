b2e673132ea054a00a4f2e974434f7a375ec63d7
Tony <tony@angel.co>
2019-05-24 10:27:59 -0400

    Rename and fix comparison

--- lib/sidekiq_utils/find_optional.rb
+++ lib/sidekiq_utils/find_optional.rb
@@ -8,7 +8,7 @@ module SidekiqUtils
     # try finding a record, but eventually give up retrying if we still cannot
     # find it. use this if you are trying to load a record from a job, but
     # don't want the failed job to end up in the RetrySet if it keeps failing.
-    def find_optional(entity, id, scope: nil, updated_at: nil)
+    def find_optional(entity, id, scope: nil, updated_since: nil)
       entity = entity.public_send(scope) if scope
       if id.is_a?(Enumerable)
         instance = entity.where(id: id)
@@ -17,7 +17,7 @@ module SidekiqUtils
       end
 
       raise(NotFoundError) unless instance.present?
-      raise(NotUpdatedError) if updated_at < instance.updated_at
+      raise(NotUpdatedError) if updated_since && instance.updated_at < updated_since
 
       instance
     end
