45a02039166908d331c5c19501f314067e0388a7
Tony <tony@oc.tc>
2019-05-24 10:28:21 -0400

    Add support for find_optional with an updated_at query (#1)
    * Add support for find_optional with an updated_at query
    
    * Rename and fix comparison

--- lib/sidekiq_utils/find_optional.rb
+++ lib/sidekiq_utils/find_optional.rb
@@ -3,22 +3,23 @@ module SidekiqUtils
     extend ActiveSupport::Concern
 
     class NotFoundError < StandardError; end
+    class NotUpdatedError < StandardError; end
 
     # try finding a record, but eventually give up retrying if we still cannot
     # find it. use this if you are trying to load a record from a job, but
     # don't want the failed job to end up in the RetrySet if it keeps failing.
-    def find_optional(entity, id, scope: nil)
+    def find_optional(entity, id, scope: nil, updated_since: nil)
       entity = entity.public_send(scope) if scope
       if id.is_a?(Enumerable)
         instance = entity.where(id: id)
       else
         instance = entity.find_by(id: id)
       end
-      if !instance.present?
-        fail(NotFoundError)
-      else
-        instance
-      end
+
+      raise(NotFoundError) unless instance.present?
+      raise(NotUpdatedError) if updated_since && instance.updated_at < updated_since
+
+      instance
     end
   end
 end
